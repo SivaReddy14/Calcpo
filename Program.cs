01a930c55e2f99d3da37c50c8b5cd82a51480e98
sivareddy <sivas14910@gmail.comgit config --global user.email sivas14910@gmail.comgit config --global user.email sivas14910@gmail.com>
2024-02-16 10:13:48 +0530

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CalcExample;
+
+Calc calc = new Calc();
+Console.WriteLine("Add call");
+calc.Add(30, 50);
+
+Console.WriteLine("Sub call");
+calc.Sub(30, 50);
+
+Console.WriteLine("Div call");
+calc.Div(40, 5);
+
+Console.WriteLine("Multi call");
+calc.Multi(3, 9);
